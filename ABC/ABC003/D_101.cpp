2b97b0631b49a6fcbb010dc80c9d2f8804f3a09c
hiro1729 <93374538+hiro1729@users.noreply.github.com>
2023-07-08 18:38:28 +0900

    一部変更

--- ABC/ABC003/D_101.cpp
+++ ABC/ABC003/D_101.cpp
@@ -20,13 +20,13 @@ void pascal_init(int n) {
 }
 
 ll t(ll x, ll y) {
-  if (x * y < l + d || x <= 0 || y <= 0) return 0;
-  return C[x * y][l + d] * C[l + d][l] % mod;
+    if (x * y < l + d || x <= 0 || y <= 0) return 0;
+    return C[x * y][l + d] * C[l + d][l] % mod;
 }
 
 int main() {
-  pascal_init(900);
-  cin >> x >> y >> h >> w >> d >> l;
-  ans = (t(h, w) - t(h - 1, w) * 2 - t(h, w - 1) * 2 + t(h - 2, w) + t(h, w - 2) + t(h - 1, w - 1) * 4 - t(h - 2, w - 1) * 2 - t(h - 1, w - 2) * 2 + t(h - 2, w - 2)) % mod;
-  cout << (ans * (x - h + 1) * (y - w + 1) % mod + mod) % mod << '\n';
+    pascal_init(900);
+    cin >> x >> y >> h >> w >> d >> l;
+    ans = (t(h, w) - t(h - 1, w) * 2 - t(h, w - 1) * 2 + t(h - 2, w) + t(h, w - 2) + t(h - 1, w - 1) * 4 - t(h - 2, w - 1) * 2 - t(h - 1, w - 2) * 2 + t(h - 2, w - 2)) % mod;
+    cout << (ans * (x - h + 1) * (y - w + 1) % mod + mod) % mod << '\n';
 }
