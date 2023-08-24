36b82313d6a84359de178e3b03e9b582ab34e994
hiro1729 <93374538+hiro1729@users.noreply.github.com>
2023-08-24 09:08:41 +0900

    a

---
+++ typical90/055/55.py
@@ -0,0 +1,12 @@
+# TLE C++ではAC
+n, p, q = map(int, input().split())
+a = list(map(int, input().split()))
+ans = 0
+for i in range(n):
+    for j in range(i):
+        for k in range(j):
+            for l in range(k):
+                for m in range(l):
+                    if a[i] * a[j] * a[k] * a[l] * a[m] % p == q:
+                        ans += 1
+print(ans)
