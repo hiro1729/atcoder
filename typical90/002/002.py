bdc2e0d5e3ece1d602d502f9b29bc62e1e50b241
hiro1729 <93374538+hiro1729@users.noreply.github.com>
2023-08-30 21:06:41 +0900

    a

---
+++ typical90/002/002.py
@@ -0,0 +1,22 @@
+def is_ok(par):
+    dep = 0
+    for i in par:
+        if i == '(':
+            dep += 1
+        else:
+            dep -= 1
+        if dep < 0:
+            return False
+    return dep == 0
+n = int(input())
+if n % 2 == 1:
+    exit(0)
+ans = []
+for i in range(1 << n):
+    par = ""
+    for j in range(n):
+        par += "()"[(i >> j) & 1]
+    if is_ok(par):
+        ans.append(par)
+for i in sorted(ans):
+    print(i)
