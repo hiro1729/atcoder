0e908e943509776216da51d57eedae77a77f4650
hiro1729 <hiroyuki@moneybee.jp>
2024-08-12 18:54:05 +0900

    Update B.py

--- ABC/ABC301-400/ABC366/B/B.py
+++ ABC/ABC301-400/ABC366/B/B.py
@@ -1,11 +1,8 @@
 N = int(input())
 S = [input() for _ in range(N)]
-L = max(len(S[i]) for i in range(N))
+L = max(map(len, S))
 for i in range(L):
 	ans = ""
-	for j in range(N - 1, -1, -1):
-		if i < len(S[j]):
-			ans += S[j][i]
-		else:
-			ans += '*'
+	for j in S[::-1]:
+		ans += j[i] if i < len(j) else '*'
 	print(ans.rstrip('*'))
